7485a3f9e3aa7d19beff6c7836738d677450c08f
Ir1d <sirius.caffrey@gmail.com>
2019-04-24 17:43:01 +0800

    enable argparse

--- submission/keras-retinanet/keras-retinanet/demo1.py
+++ submission/keras-retinanet/keras-retinanet/demo1.py
@@ -22,6 +22,7 @@ import os
 import numpy as np
 import pandas as pd
 import time
+import argparse
 
 # set tf backend to allow memory to grow, instead of claiming everything
 import tensorflow as tf
@@ -53,9 +54,23 @@ model = models.load_model(model_path, backbone_name='resnet50')
 # load label to names mapping for visualization purposes
 labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
 
-
-imagedir = '/images'
-resdir = '/predictions'
+parser = argparse.ArgumentParser(description='keras demo')
+parser.add_argument('--data_dir',
+                    type=str, default='/images',
+                    help='directory to load data')
+parser.add_argument('--res_dir',
+                    type=str, default='/predictions',
+                    help='directory to save res')
+args = parser.parse_args()
+if not os.path.exists(args.res_dir):
+    os.makedirs(args.res_dir)
+if not os.path.exists(args.data_dir):
+    print(args.data_dir, ' not found!')
+    exit
+imagedir = args.data_dir
+resdir = args.res_dir
+print('data_dir', args.data_dir)
+print('res_dir', args.res_dir)
 
 for imagename in os.listdir(imagedir):
     if imagename.endswith('png') or imagename.endswith('jpg'):
